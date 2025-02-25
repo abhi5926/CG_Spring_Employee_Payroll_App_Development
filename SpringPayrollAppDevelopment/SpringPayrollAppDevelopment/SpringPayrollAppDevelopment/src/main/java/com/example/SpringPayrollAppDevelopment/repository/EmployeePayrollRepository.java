3a12f5cc6e342dfc7667e291d2041329976309a4
abhi5926 <abhisheksen5926@gmail.com>
2025-02-25 19:24:47 +0530

    [Abhishek] Add. Introducing Model to Employee Payroll App

--- SpringPayrollAppDevelopment/SpringPayrollAppDevelopment/SpringPayrollAppDevelopment/src/main/java/com/example/SpringPayrollAppDevelopment/repository/EmployeePayrollRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.SpringPayrollAppDevelopment.repository;
-
-import com.example.SpringPayrollAppDevelopment.model.EmployeePayroll;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeePayrollRepository extends JpaRepository<EmployeePayroll,Integer> {
-}
