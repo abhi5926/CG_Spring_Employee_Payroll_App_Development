64cc09f75646e8f7c68f66079f6ffc182245a9d5
abhi5926 <abhisheksen5926@gmail.com>
2025-02-25 13:14:26 +0530

    [Abhishek] Add. Employee Payroll App and Creating Rest Controller to Demonstrate the Various HTTP Methods

---
+++ SpringPayrollAppDevelopment/SpringPayrollAppDevelopment/src/main/java/com/example/SpringPayrollAppDevelopmentApplication.java
@@ -0,0 +1,14 @@
+package com.example.SpringPayrollAppDevelopment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+SpringPayrollAppDevelopmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPayrollAppDevelopmentApplication.class, args);
+	}
+
+}
