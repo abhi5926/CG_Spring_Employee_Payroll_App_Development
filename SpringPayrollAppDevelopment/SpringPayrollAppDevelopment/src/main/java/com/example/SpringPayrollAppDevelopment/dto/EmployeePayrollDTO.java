592b176b0940256701f4efff3600f3f307b57f37
abhi5926 <abhisheksen5926@gmail.com>
2025-03-02 19:00:07 +0530

    [Abhishek] Add. Employee Payroll App Adding Validation Field so the Rest Call can be Validated

---
+++ SpringPayrollAppDevelopment/SpringPayrollAppDevelopment/src/main/java/com/example/SpringPayrollAppDevelopment/dto/EmployeePayrollDTO.java
@@ -0,0 +1,29 @@
+package com.example.SpringPayrollAppDevelopment.dto;
+
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+import lombok.Data;
+
+
+@Data
+public class EmployeePayrollDTO {
+    @NotEmpty(message = "Name cannot be empty")
+    @Pattern(regexp = "^[A-Z][a-zA-Z]{2,}$", message = "Name must start with a capital letter and be at least 3 characters long")
+    private String name;
+
+    @Min(value = 1000, message = "Salary must be at least 1000")
+    @Max(value = 1000000, message = "Salary cannot exceed 1,000,000")
+    private int salary;
+    public EmployeePayrollDTO(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "EmployeePayrollDTO{name='" + name + "', salary=" + salary + "}";
+    }
+}
